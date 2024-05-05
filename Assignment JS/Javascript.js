a6c33065d5b652d6c7fd9e0d21b1c03888a1ee08
lovelyjibs <167642085+lovelyjibs@users.noreply.github.com>
2024-05-05 17:46:13 +0100

    Add files via upload

---
+++ Assignment JS/Javascript.js
@@ -0,0 +1,42 @@
+
+
+
+length = 10
+width =5
+
+//Calculate the are of a rectangle
+
+function calculateArea(lenght, width) {
+    const area = 10 * 5
+    return area;
+}
+
+console.log( 10 * 5)
+
+
+
+ //Create a function called Reverse of that String
+
+ const myOriginalString = "Jigbale"
+
+ const myReverseString =  "Elabgij"
+
+ console.log(myOriginalString)
+
+ console.log(myReverseString);
+
+ 
+ //Write a JavaScript function named isEven that takes a number as input and returns true if the number is even (i.e. divisible by 2 without remainder), otherwise returns false.
+ 
+
+
+ function isEven (number) {
+    return Math.floor(number / 2) * 2 === number;
+
+
+}
+
+console.log(isEven(9));
+console.log(isEven(14));
+console.log(isEven(100));
+console.log(isEven(551));
